e035fc441c9835f20dafe195c22e35645e4b2e6d
jameslfc19 <jameslfc19@gmail.com>
2013-05-24 21:35:35 +0100

    Vanilla worldgen
    Signed-off-by: jameslfc19 <jameslfc19@gmail.com>

--- src/org/codeliners/mod/oreliners/generators/Vanilla.java
+++ src/org/codeliners/mod/oreliners/generators/Vanilla.java
@@ -1,7 +1,10 @@
 package org.codeliners.mod.oreliners.generators;
 
+import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+
 import org.codeliners.mod.oreliners.api.IGenerator;
 import org.w3c.dom.Element;
 
@@ -17,6 +20,28 @@ public class Vanilla implements IGenerator{
 
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider, Map<String, Map<String, String>> options) {
+    
+    	int minheight = Integer.parseInt(options.get("oreheight").get("min"));
+    	int maxheight = Integer.parseInt(options.get("oreheight").get("max"));
+    	int veinsize = Integer.parseInt(options.get("veinsize").get("max"));
+    	int spawnratepercent = Integer.parseInt(options.get("spawnrate").get("value"));
+    	int BLOCKID = 0;
+    	
+    		int spawnrate = ((((((maxheight-minheight)*16)*16)/100)*spawnratepercent)/veinsize);
+    		
+    		for(int a = 0; a < spawnrate; a++){
+    			int XCoord = chunkX*16 + random.nextInt(16);
+            	int YCoord = random.nextInt(maxheight-minheight)+minheight;
+            	int ZCoord = chunkZ*16 + random.nextInt(16);
+            	
+            	if(world.getBlockId(XCoord, YCoord, ZCoord) == Block.stone.blockID){
+            		(new WorldGenMinable(BLOCKID, veinsize)).generate(world, random, XCoord, YCoord, ZCoord);
+            	}
+
+    		}
+    		
+    		
+    
     }
 
 }
