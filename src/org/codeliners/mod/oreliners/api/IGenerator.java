d45c369c616d9c8ec4385de410b691ea7c2f5bf1
Stiepen <stiepen22@gmx.de>
2013-05-24 22:37:52 +0200

    Added some hooks to IGenerator

--- src/org/codeliners/mod/oreliners/api/IGenerator.java
+++ src/org/codeliners/mod/oreliners/api/IGenerator.java
@@ -1,12 +1,16 @@
 package org.codeliners.mod.oreliners.api;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import org.codeliners.mod.oreliners.api.config.IParserAccess;
 
 import java.util.Map;
 import java.util.Random;
 
 public interface IGenerator {
 	String getName();
-	void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider, Map<String, Map<String, String>> options);
+	void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator,
+                  IChunkProvider chunkProvider, Map<String, Map<String, String>> options, IParserAccess parser,
+                  ItemStack block);
 }
